10cef02c00551b6a2d76759d1d19983dc5c9388e
gotokouichi <koichi.110913@icloud.com>
2021-04-01 17:11:51 +0900

    初期表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,39 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Client, { Product } from 'shopify-buy'
+import { GetStaticProps } from 'next'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+type IndexProps = {
+  products: Product[];
+};
+
+const client = Client.buildClient({
+  domain: 'nexjs-hikoymama.myshopify.com',　//自分のストアのURLを入力する
+  storefrontAccessToken: 'e4e4011cbbd8610a19a643c8cccf5537', //自分のStorefront APIのアクセストークンを入力する
+});
+
+const IndexPage: React.FC<IndexProps> = ({ products }) => {
+  return (
+    <>
+      <h1>Hello Next.js 👋</h1>
+      <ul>
+        {
+          products.map((product) => 
+          <li key={product.id}>
+            {product.title}
+            <img src={product.images[0].src} height={80}/>
+          </li>)
+        }
+      </ul>
+    </>
+  );
+}
 
 export default IndexPage
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products: any = await client.product.fetchAll();
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    },
+  };
+};
